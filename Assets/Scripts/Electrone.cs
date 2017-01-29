2e46e7ce1747a0db7333f573832717485cb52f99
hendmahmoud <hend.com13@gmail.com>
2017-01-29 19:11:05 +0200

    hend's empty classes

---
+++ Assets/Scripts/Electrone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Electrone : ScriptableObject {
+	GameObject sphere;
+	// Use this for initialization
+	public Electrone (Vector3 pos) {
+		sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = pos;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Nucleus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nucleus : MonoBehaviour {
+	
+	private ArrayList brotones;
+	private ArrayList netrones;
+
+	public Nucleus (int numOfProtones,int numOfNeutrones){
+		brotones = new ArrayList ();
+		netrones = new ArrayList ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
